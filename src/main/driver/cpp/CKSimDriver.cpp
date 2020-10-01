159969e857c8e57a6bc9faae92a1315dacd4391d
Robert Hilton <robert.a.hilton.jr@gmail.com>
2020-10-01 11:10:41 -0400

    Grab all topics

--- src/main/driver/cpp/CKSimDriver.cpp
+++ src/main/driver/cpp/CKSimDriver.cpp
@@ -9,7 +9,8 @@ namespace ck
     //const std::string ZMQ_SERVER_IP = "10.1.95.199";
     const std::string ZMQ_REQREP_SERVER_PORT = "10501";
     const std::string ZMQ_PUBSUB_SERVER_PORT = "10502";
-    const std::string TOPIC = "10000";
+    // const std::string TOPIC = "10000";
+    const std::string TOPIC = "";
 
     static void *contextSubscriber;
     static void *subscriberSocket;
