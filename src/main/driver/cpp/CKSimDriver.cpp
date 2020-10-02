ae09730dd19de6921e7c2a576214650f0ad38684
Robert Hilton <robert.a.hilton.jr@gmail.com>
2020-10-02 00:05:22 -0400

    Add send and recv daemons

--- src/main/driver/cpp/CKSimDriver.cpp
+++ src/main/driver/cpp/CKSimDriver.cpp
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <iostream>
 #include <stdlib.h>
+#include <hal/HAL.h>
 #include <wpi/mutex.h>
 #include <wpi/SafeThread.h>
 
@@ -19,6 +20,59 @@
 
 namespace ck
 {
+    static void *contextSubscriber;
+    static void *subscriberSocket;
+    int ZMQSubInit()
+    {
+        int socketRecvTimeout = 1000;
+        contextSubscriber = zmq_ctx_new();
+        subscriberSocket = zmq_socket(contextSubscriber, ZMQ_SUB);
+        int retVal = 0;
+        if ((retVal = zmq_connect(subscriberSocket, BuildConnStr(ZMQ_SERVER_IP, ZMQ_PUBSUB_SERVER_PORT).c_str())) > -1)
+        {
+            retVal = zmq_setsockopt(subscriberSocket, ZMQ_SUBSCRIBE, TOPIC.c_str(), TOPIC.length());
+            retVal |= zmq_setsockopt(subscriberSocket, ZMQ_RCVTIMEO, &socketRecvTimeout, sizeof(int));
+        }
+        return retVal;
+    }
+
+    static void *contextRequester;
+    static void *requesterSocket;
+    int ZMQReqInit()
+    {
+        int socketRecvTimeout = 1000;
+        contextRequester = zmq_ctx_new();
+        requesterSocket = zmq_socket(contextRequester, ZMQ_REQ);
+        int retVal = 0;
+        if ((retVal = zmq_connect(requesterSocket, BuildConnStr(ZMQ_SERVER_IP, ZMQ_REQREP_SERVER_PORT).c_str())) > -1)
+        {
+            retVal = zmq_setsockopt(requesterSocket, ZMQ_SNDTIMEO, &socketRecvTimeout, sizeof(int));
+            retVal |= zmq_setsockopt(requesterSocket, ZMQ_RCVTIMEO, &socketRecvTimeout, sizeof(int));
+        }
+        return retVal;
+    }
+
+    int ZMQSubRecvTest()
+    {
+        char buf[1000] = "";
+        zmq_msg_t in_msg;
+        zmq_msg_init(&in_msg);
+        int numBytes = 0;
+        if ((numBytes = zmq_recvmsg(subscriberSocket, &in_msg, ZMQ_NULL)) > -1)
+        {
+            if (int size = zmq_msg_size(&in_msg))
+            {
+                memcpy(buf, zmq_msg_data(&in_msg), size);
+                zmq_msg_close(&in_msg);
+                buf[size] = 0;
+                std::cout << "Got " << numBytes << " bytes!" << std::endl;
+                std::cout << "MsgSize " << size << " bytes!" << std::endl;
+                std::cout << buf << std::endl;
+            }
+        }
+        return numBytes;
+    }
+
     static wpi::mutex controlMsgMutex;
     static ControlMessage controlMessage;
     void ControlMessageInit()
@@ -31,16 +85,20 @@ namespace ck
             v->set_value(0);
         }
     }
-    float GetMotor(int id) {
+    float GetMotor(int id)
+    {
         std::lock_guard<wpi::mutex> lock(controlMsgMutex);
-        if (id >= 0 && id < MAX_NUM_MOTORS) {
+        if (id >= 0 && id < MAX_NUM_MOTORS)
+        {
             return controlMessage.motors(id).value();
         }
         return 0;
     }
-    void SetMotor(int id, float val) {
+    void SetMotor(int id, float val)
+    {
         std::lock_guard<wpi::mutex> lock(controlMsgMutex);
-        if (id >= 0 && id < MAX_NUM_MOTORS) {
+        if (id >= 0 && id < MAX_NUM_MOTORS)
+        {
             controlMessage.mutable_motors(id)->set_value(val);
         }
     }
@@ -69,46 +127,57 @@ namespace ck
             v->set_value(0);
         }
     }
-    float GetEncoder(int id) {
+    float GetEncoder(int id)
+    {
         std::lock_guard<wpi::mutex> lock(statusMsgMutex);
-        if (id >= 0 && id < MAX_NUM_MOTORS) {
+        if (id >= 0 && id < MAX_NUM_MOTORS)
+        {
             return statusMessage.encoders(id).value();
         }
         return 0;
     }
-    void SetEncoder(int id, float val) {
+    void SetEncoder(int id, float val)
+    {
         std::lock_guard<wpi::mutex> lock(statusMsgMutex);
-        if (id >= 0 && id < MAX_NUM_MOTORS) {
+        if (id >= 0 && id < MAX_NUM_MOTORS)
+        {
             statusMessage.mutable_encoders(id)->set_value(val);
         }
     }
-    float GetAccelerometer(int id) {
+    float GetAccelerometer(int id)
+    {
         std::lock_guard<wpi::mutex> lock(statusMsgMutex);
-        if (id >= 0 && id < MAX_NUM_ACCEL) {
+        if (id >= 0 && id < MAX_NUM_ACCEL)
+        {
             return statusMessage.accelerometers(id).value();
         }
         return 0;
     }
-    void SetAccelerometer(int id, float val) {
+    void SetAccelerometer(int id, float val)
+    {
         std::lock_guard<wpi::mutex> lock(statusMsgMutex);
-        if (id >= 0 && id < MAX_NUM_ACCEL) {
+        if (id >= 0 && id < MAX_NUM_ACCEL)
+        {
             statusMessage.mutable_accelerometers(id)->set_value(val);
         }
     }
-    float GetGyro(int id) {
+    float GetGyro(int id)
+    {
         std::lock_guard<wpi::mutex> lock(statusMsgMutex);
-        if (id >= 0 && id < MAX_NUM_GYRO) {
+        if (id >= 0 && id < MAX_NUM_GYRO)
+        {
             return statusMessage.gyroscopes(id).value();
         }
         return 0;
     }
-    void SetGyro(int id, float val) {
+    void SetGyro(int id, float val)
+    {
         std::lock_guard<wpi::mutex> lock(statusMsgMutex);
-        if (id >= 0 && id < MAX_NUM_GYRO) {
+        if (id >= 0 && id < MAX_NUM_GYRO)
+        {
             statusMessage.mutable_gyroscopes(id)->set_value(val);
         }
     }
-
 
     class CommDaemonSend : public wpi::SafeThread
     {
@@ -117,7 +186,38 @@ namespace ck
         ~CommDaemonSend() {}
 
     private:
-        void Main() override {}
+        void Main() override
+        {
+            constexpr uint64_t kThreadRate = 10000; //10ms, units in usec
+            int32_t status;
+            HAL_NotifierHandle m_setNotifier = HAL_InitializeNotifier(&status);
+            uint64_t currTime;
+            uint64_t startTime = HAL_GetFPGATime(&status);
+            char bufRecvArr[128];
+            char bufSendArr[4096];
+            memset(bufRecvArr, 0, sizeof(bufRecvArr));
+            memset(bufSendArr, 0, sizeof(bufSendArr));
+            size_t dataSize = 0;
+            while (m_active)
+            {
+                std::unique_lock<wpi::mutex> lock(controlMsgMutex);
+                if (!controlMessage.SerializeToArray(bufSendArr, sizeof(bufSendArr)))
+                {
+                    continue;
+                }
+                dataSize = controlMessage.ByteSizeLong();
+                lock.unlock();
+
+                zmq_send(requesterSocket, bufSendArr, dataSize, ZMQ_NULL);
+                zmq_recv(requesterSocket, bufRecvArr, sizeof(bufRecvArr), ZMQ_NULL);
+                currTime = HAL_GetFPGATime(&status);
+                HAL_UpdateNotifierAlarm(m_setNotifier, (uint64_t)(currTime + (kThreadRate - (currTime - startTime))), &status);
+                HAL_WaitForNotifierAlarm(m_setNotifier, &status);
+                startTime = HAL_GetFPGATime(&status);
+            }
+            HAL_StopNotifier(m_setNotifier, &status);
+            HAL_CleanNotifier(m_setNotifier, &status);
+        }
     };
 
     class CommDaemonRecv : public wpi::SafeThread
@@ -127,54 +227,26 @@ namespace ck
         ~CommDaemonRecv() {}
 
     private:
-        void Main() override {}
-    };
-
-    static void *contextSubscriber;
-    static void *subscriberSocket;
-    int ZMQSubInit()
-    {
-        int socketRecvTimeout = 1000;
-        contextSubscriber = zmq_ctx_new();
-        subscriberSocket = zmq_socket(contextSubscriber, ZMQ_SUB);
-        int retVal = 0;
-        if ((retVal = zmq_connect(subscriberSocket, BuildConnStr(ZMQ_SERVER_IP, ZMQ_PUBSUB_SERVER_PORT).c_str())) > -1)
+        void Main() override
         {
-            retVal = zmq_setsockopt(subscriberSocket, ZMQ_SUBSCRIBE, TOPIC.c_str(), TOPIC.length());
-            retVal |= zmq_setsockopt(subscriberSocket, ZMQ_RCVTIMEO, &socketRecvTimeout, sizeof(int));
-        }
-        return retVal;
-    }
-
-    static void *contextRequester;
-    static void *requesterSocket;
-    int ZMQReqInit()
-    {
-        contextRequester = zmq_ctx_new();
-        requesterSocket = zmq_socket(contextRequester, ZMQ_REQ);
-        return zmq_connect(requesterSocket, BuildConnStr(ZMQ_SERVER_IP, ZMQ_REQREP_SERVER_PORT).c_str());
-    }
-
-    int ZMQSubRecvTest()
-    {
-        char buf[1000] = "";
-        zmq_msg_t in_msg;
-        zmq_msg_init(&in_msg);
-        int numBytes = 0;
-        if ((numBytes = zmq_recvmsg(subscriberSocket, &in_msg, ZMQ_NULL)) > -1)
-        {
-            if (int size = zmq_msg_size(&in_msg))
+            char bufRecvArr[4096];
+            memset(bufRecvArr, 0, sizeof(bufRecvArr));
+            while (m_active)
             {
-                memcpy(buf, zmq_msg_data(&in_msg), size);
-                zmq_msg_close(&in_msg);
-                buf[size] = 0;
-                std::cout << "Got " << numBytes << " bytes!" << std::endl;
-                std::cout << "MsgSize " << size << " bytes!" << std::endl;
-                std::cout << buf << std::endl;
+                int dataSize = 0;
+                if ((dataSize = zmq_recv(subscriberSocket, bufRecvArr, sizeof(bufRecvArr), ZMQ_NULL)) > 0)
+                {
+                    StatusMessage tmpStatusMessage;
+                    if (tmpStatusMessage.ParseFromArray(bufRecvArr, dataSize))
+                    {
+                        std::unique_lock<wpi::mutex> lock(statusMsgMutex);
+                        statusMessage = tmpStatusMessage;
+                        lock.unlock();
+                    }
+                }
             }
         }
-        return numBytes;
-    }
+    };
 
     void CKSimDealloc()
     {
