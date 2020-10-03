5e0573bc0f762ac507ccc860662ba716cfacf72a
Robert Hilton <robert.a.hilton.jr@gmail.com>
2020-10-02 23:02:55 -0400

    Update make external cmd

--- make_external.cmd
+++ make_external.cmd
@@ -1,18 +1,25 @@
 @echo off
+set "rootDir=%cd%"
+cd src\main\driver\include
+rmdir /S /Q protobuf
+mkdir protobuf
+cd "%rootDir%\src\main\driver\cpp"
+rmdir /S /Q protobuf
+mkdir protobuf
+cd "%rootDir%"
 rmdir /S /Q generated
 mkdir generated
+cd external\CKSimProtoSpec
 for %%f in (*.proto) do (
     echo %%~nf
-	..\CKSimLibCpp\lib\protobuf\protoc.exe %%~nf.proto --cpp_out=generated
+	"%rootDir%\lib\protobuf\protoc.exe" %%~nf.proto --cpp_out="%rootDir%\generated"
 )
-cd generated
-mkdir include
-mkdir src
+cd "%rootDir%\generated"
 for %%f in (*.cc) do (
-	move %%~nf.cc src/ > nul
+	move %%~nf.cc "%rootDir%\src\main\driver\cpp\protobuf\" > nul
 )
 for %%f in (*.h) do (
-	move %%~nf.h include/ > nul
+	move %%~nf.h "%rootDir%\src\main\driver\include\protobuf\" > nul
 )
-cd ..
+cd "%rootDir%"
 echo Generated!
